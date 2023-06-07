54da8ed7b2c5afc147d927d96b999fb04d1c8c3d
Serhii Klokun <klokuns@gmail.com>
2023-06-07 19:58:55 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ import { Title, Div, TitleList } from './App.styled';
 import { useLocalStorage } from '../huks/useLocalStorage';
 import { toastOptions } from '../options/toastOptions';
 
-const defaultValue = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const defaultValue = [];
 
 const App = () => {
   const [filter, setFilter] = useState('');
