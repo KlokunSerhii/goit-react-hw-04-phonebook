1b9b3bbb15ecb286afcf41a1e698db610e34ad22
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-06-07 10:46:46 +0000

    Local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import { nanoid } from 'nanoid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,23 +6,20 @@ import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 import { Title, Div, TitleList } from './App.styled';
+import {useLocalStorage} from '../huks/huks'
+
+const defaultValue = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
 
 const App = () => {
   const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
+  const [contacts, setContacts] = useLocalStorage('contact', defaultValue );
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+
 
   const handleSubmit = ({ name, number }) => {
     const newContacts = {
@@ -33,7 +30,6 @@ const App = () => {
     const find = contacts.find(
       element => element.name.toLowerCase() === name.toLowerCase()
     );
-
     find
       ? alert(find.name + ' is already in contacts.')
       : setContacts(prevContacts => [newContacts, ...prevContacts]);
